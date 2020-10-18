490162c8a4206b77b867f4d458e830a038d61069
NO ONE <38581702+HACKER097@users.noreply.github.com>
2020-10-18 19:45:31 +0530

    Update finder.py

--- finder.py
+++ finder.py
@@ -72,11 +72,10 @@ while True:
 				for line in lines:
 					print(line)
 					line = line[:12].split(":")
-					print(line)
 					try:
 						time = round(int(line[2])*60 + float(line[3]))
 					except IndexError:
-						print("WORKING ON A FIX")
+						print("***WORKING ON A FIX***")
 
 					print()
 					print("youtube link : https://youtube.com/watch?v=" + link + "&t=" + str(time) + "s")
